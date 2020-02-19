a7296397ebf8f27bb2a66c8a8a206ec9660cc5ee
orpartz <orptzz@gmail.com>
2020-02-19 20:32:24 +0200

    https fix

--- js/services/weather-service.js
+++ js/services/weather-service.js
@@ -3,7 +3,7 @@ export default {
 }
 
 function getForcast(coords) {
-	return axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lng}&appid=bb92276d35d1215a8ad73c28452671a0`)
+	return axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lng}&appid=bb92276d35d1215a8ad73c28452671a0`)
 		.then(res => {
 			return {
 				location: `${res.data.name} ${res.data.sys.country}`, 
